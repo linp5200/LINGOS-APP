5193bc02fb653ed03bb4a34b09de33ce6cb52a6c
linp5200 <linp5200@users.noreply.github.com>
2026-08-14 21:30:06 +0800

    fix: sync_settings_screen import 路径（screens 子目录需 ../../../core）

--- lib/features/ai_config/screens/sync_settings_screen.dart
+++ lib/features/ai_config/screens/sync_settings_screen.dart
@@ -9,9 +9,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../core/providers.dart';
-import '../../core/storage/app_store.dart';
-import '../../core/theme/app_theme.dart';
+import '../../../core/providers.dart';
+import '../../../core/storage/app_store.dart';
+import '../../../core/theme/app_theme.dart';
 
 class SyncSettingsScreen extends ConsumerStatefulWidget {
   const SyncSettingsScreen({super.key});
