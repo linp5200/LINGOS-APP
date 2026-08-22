342bc476185cff711a3dec9c32515e7858886bce
linp5200 <you15016554133@163.com>
2026-08-23 01:40:27 +0800

    0.2.4 CI 修复：kotlinOptions 弃用→compilerOptions DSL（Kotlin 2.3）

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,7 +23,9 @@ subprojects {
         targetCompatibility = "17"
     }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 }
 
